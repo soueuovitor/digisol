cf4ea65fcb3d0a2fa7fda92ff9634dce5d98ef23
vitormiegsi <vhoa1995@hotmail.com>
2018-04-10 00:57:34 +0100

    Nw

--- controllers/activities.route.js
+++ controllers/activities.route.js
@@ -1,58 +1,58 @@
 const express = require('express');
 const router = express.Router();
 const model = require('../models/activities.model')
-router.get('/', function (request, response) {
+router.get('/', function(request, response) {
 	//console.log(request.isAuthenticated());
 
-	model.listClientes(function (clients) {
-		model.listTipos(function (tipos) {
+	model.listClientes(function(clients) {
+		model.listTipos(function(tipos) {
 
-		model.listUsers(function (users) {
-			model.listActividades(function (activities) {
+			model.listUsers(function(users) {
+				model.listActividades(function(activities) {
 
-			response.set("Content-Type", "text/html");
-			response.render('./activities', {
-				clients: clients,
-				users: users,
-				activities : activities,
-				tipos : tipos
+					response.set("Content-Type", "text/html");
+					response.render('./activities', {
+						clients: clients,
+						users: users,
+						activities: activities,
+						tipos: tipos
+					})
+				})
 			})
 		})
-		})
-	})
 
 	});
 
 });
 
-router.post('/fim', function(request, response){
-/*
-	Number.prototype.toTime = function(isSec) {
-		var ms = isSec ? this * 1e3 : this,
-			lm = ~(4 * !!isSec),  
-			fmt = new Date(ms).toISOString().slice(11, lm);
-	
-		if (ms >= 8.64e7) {  
-			var parts = fmt.split(/:(?=\d{2}:)/);
-			parts[0] -= -24 * (ms / 8.64e7 | 0);
-			return parts.join(':');
-		}
-	
-		return fmt;
-	};
+router.post('/fim', function(request, response) {
+	/*
+		Number.prototype.toTime = function(isSec) {
+			var ms = isSec ? this * 1e3 : this,
+				lm = ~(4 * !!isSec),  
+				fmt = new Date(ms).toISOString().slice(11, lm);
+		
+			if (ms >= 8.64e7) {  
+				var parts = fmt.split(/:(?=\d{2}:)/);
+				parts[0] -= -24 * (ms / 8.64e7 | 0);
+				return parts.join(':');
+			}
+		
+			return fmt;
+		};
 
-	var horaInicio = new Date(request.body.timeStart).getTime();
+		var horaInicio = new Date(request.body.timeStart).getTime();
 
 
-// Apply each element to the Date function
-	var horaFim = new Date(request.body.timeEnd).getTime();
+	// Apply each element to the Date function
+		var horaFim = new Date(request.body.timeEnd).getTime();
 
-	var seconds = (horaFim - horaInicio) / 1000;
+		var seconds = (horaFim - horaInicio) / 1000;
 
-	console.log(seconds);
-var duracao = (seconds .toTime(true))
-console.log(duracao);
-*/
+		console.log(seconds);
+	var duracao = (seconds .toTime(true))
+	console.log(duracao);
+	*/
 	var data = {
 
 		'id': request.body.id,
@@ -61,50 +61,78 @@ console.log(duracao);
 	};
 
 
-	model.fimAct(data, function () {});
+	model.fimAct(data, function() {});
 
 
 
 
 });
 
-router.post('/increment', function(request, response){
+router.post('/increment', function(request, response) {
 
+	Date.prototype.getWeekNumber = function() {
+		var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+		var dayNum = d.getUTCDay() || 7;
+		d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+		var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+		return Math.ceil((((d - yearStart) / 86400000) + 1) / 7)
+	};
 	Number.prototype.toTime = function(isSec) {
 		var ms = isSec ? this * 1e3 : this,
-			lm = ~(4 * !!isSec),  /* limit fraction */
+			lm = ~(4 * !!isSec),
+			/* limit fraction */
 			fmt = new Date(ms).toISOString().slice(11, lm);
-	
-		if (ms >= 8.64e7) {  /* >= 24 hours */
+
+		if (ms >= 8.64e7) { /* >= 24 hours */
 			var parts = fmt.split(/:(?=\d{2}:)/);
 			parts[0] -= -24 * (ms / 8.64e7 | 0);
 			return parts.join(':');
 		}
-	
+
 		return fmt;
 	};
+
+
+	Date.prototype.getWeekOfMonth = function(exact) {
+		var month = this.getMonth(),
+			year = this.getFullYear(),
+			firstWeekday = new Date(year, month, 1).getDay(),
+			lastDateOfMonth = new Date(year, month + 1, 0).getDate(),
+			offsetDate = this.getDate() + firstWeekday - 1,
+			index = 1 // start index at 0 or 1, your choice
+			,
+			weeksInMonth = index + Math.ceil((lastDateOfMonth + firstWeekday - 7) / 7),
+			week = index + Math.floor(offsetDate / 7);
+		if (exact || week < 2 + index) return week;
+		return week === weeksInMonth ? index + 5 : week;
+	};
+
 	var horaInicio = new Date(request.body.start).getTime();
-
-
-// Apply each element to the Date function
+	var inicio = new Date(request.body.start);
+	var week = inicio.getWeekOfMonth(true);
+	var month = inicio.getMonth() + 1;
+	// Apply each element to the Date function
 	var horaFim = new Date(request.body.end).getTime();
-
+	
 	var seconds = (horaFim - horaInicio) / 1000;
 
-	console.log(seconds);
-var duracao = (seconds .toTime(true))
-console.log(duracao);
+	console.log(month);
+	var duracao = (seconds.toTime(true))
+	console.log(duracao);
 
 	var data = {
 
 		'id': request.body.id,
 		'start': request.body.start,
 		'end': request.body.end,
-		'duration': duracao
+		'duration': duracao,
+		'week': week,
+		'month': month,
+		'user': request.body.user
 	};
 
 
-	model.increment(data, function () {});
+	model.increment(data, function() {});
 
 
 
@@ -112,7 +140,7 @@ console.log(duracao);
 });
 
 
-router.post('/restart', function(request, response){
+router.post('/restart', function(request, response) {
 
 
 
@@ -121,15 +149,15 @@ router.post('/restart', function(request, response){
 
 		'id': request.body.id,
 
-		
+
 	};
-	model.restartAct(data, function () {});
+	model.restartAct(data, function() {});
 
 
 
 
 });
-router.post('/inicio', function(request, response){
+router.post('/inicio', function(request, response) {
 
 
 
@@ -140,14 +168,14 @@ router.post('/inicio', function(request, response){
 
 		'time': request.body.time
 	};
-	model.inicioAct(data, function () {});
+	model.inicioAct(data, function() {});
 
 
 
 
 });
 
-router.post('/newToDo', function(request, response){
+router.post('/newToDo', function(request, response) {
 
 
 
@@ -161,7 +189,7 @@ router.post('/newToDo', function(request, response){
 		'tipo': request.body.tipo,
 		'sub': request.body.subtipo
 	};
-	model.createToDo(data, function () {});
+	model.createToDo(data, function() {});
 
 
 	response.json({
--- models/activities.model.js
+++ models/activities.model.js
@@ -5,7 +5,7 @@ module.exports = {
 
 	listClientes(callback) {
 		var sql = 'SELECT * from clientes';
-		global.connection.query(sql, function (error, rows, fields) {
+		global.connection.query(sql, function(error, rows, fields) {
 			if (error) throw error;
 			callback(rows);
 
@@ -13,56 +13,75 @@ module.exports = {
 	},
 	listTipos(callback) {
 		var sql = 'SELECT * from tipo';
-		global.connection.query(sql, function (error, rows, fields) {
+		global.connection.query(sql, function(error, rows, fields) {
 			if (error) throw error;
 			callback(rows);
 
 		});
-    },
+	},
+	listHoras(callback) {
+		var sql = 'SELECT * from horas';
+		global.connection.query(sql, function(error, rows, fields) {
+			if (error) throw error;
+			callback(rows);
+
+		});
+	},
+
+	listHorasByUser(id_user, callback) {
+		var sql = 'SELECT * from horas WHERE id_utilizador=?';
+		global.connection.query(sql, [ id_user ],
+			function(error, rows, fields) {
+			if (error) throw error;
+			callback(rows);
+
+		});
+	},
+
 	listActividades(callback) {
 		var sql = 'SELECT * from actividades';
-		global.connection.query(sql, function (error, rows, fields) {
+		global.connection.query(sql, function(error, rows, fields) {
 			if (error) throw error;
 			callback(rows);
 
 		});
-    },
-    
-    listUsers(callback) {
+	},
+
+	listUsers(callback) {
 		var sql = 'SELECT * from utilizadores';
-		global.connection.query(sql, function (error, rows, fields) {
+		global.connection.query(sql, function(error, rows, fields) {
 			if (error) throw error;
 			callback(rows);
 
 		});
-    },
-    inicioAct(data,callback){
+	},
+	inicioAct(data, callback) {
 		var sql = "UPDATE actividades SET hora_inicio=? WHERE id_actividade=?";
 		global.connection.query(
-			sql, [data.time,  data.id],
-			function (error, rows, fields) {
+			sql, [data.time, data.id],
+			function(error, rows, fields) {
 				if (error) throw error;
 				callback(rows[0]);
 			});
 
 
 	},
-	fimAct(data,callback){
+	fimAct(data, callback) {
 		var sql = "UPDATE actividades SET hora_fim=?  WHERE id_actividade=?";
 		global.connection.query(
 			sql, [data.time, data.id],
-			function (error, rows, fields) {
+			function(error, rows, fields) {
 				if (error) throw error;
 				callback(rows[0]);
 			});
 
 
 	},
-	increment(data, callback){
-		var sql = "INSERT INTO horas (actividade,duracao_sessao, inicio_sessao, fim_sessao) VALUES (?,?,?,?)";
+	increment(data, callback) {
+		var sql = "INSERT INTO horas (actividade,duracao_sessao, inicio_sessao, fim_sessao, mes, semana, id_utilizador) VALUES (?,?,?,?,?,?,?)";
 		global.connection.query(
-			sql, [data.id, data.duration, data.start, data.end],
-			function (error, rows, fields) {
+			sql, [data.id, data.duration, data.start, data.end, data.month, data.week, data.user],
+			function(error, rows, fields) {
 				if (error) throw error;
 				callback(rows[0]);
 			});
@@ -73,22 +92,22 @@ module.exports = {
 
 	},
 
-	restartAct(data,callback){
+	restartAct(data, callback) {
 		var sql = "UPDATE actividades SET hora_fim=0 , hora_inicio=0 , duracao=0 WHERE id_actividade=?";
 		global.connection.query(
 			sql, [data.id],
-			function (error, rows, fields) {
+			function(error, rows, fields) {
 				if (error) throw error;
 				callback(rows[0]);
 			});
 
 
 	},
-	createToDo(data, callback){
+	createToDo(data, callback) {
 		var sql = "INSERT INTO actividades (nome_actividade, id_cliente_fk, id_utilizador, descricao,tipo,subtipo) VALUES (?,?,?,?,?,?)";
 		global.connection.query(
 			sql, [data.titulo, data.cliente, data.user, data.descricao, data.tipo, data.sub],
-			function (error, rows, fields) {
+			function(error, rows, fields) {
 				if (error) throw error;
 				callback(rows[0]);
 			});
@@ -104,7 +123,7 @@ module.exports = {
 		var sql = "INSERT INTO clientes (nome, morada, telemovel, email, nif, cc, num_fotos_cliente, data_nascimento) VALUES (?,?,?,?,?,?,?,?)";
 		global.connection.query(
 			sql, [data.nome, data.morada, data.telemovel, data.email, data.nif, data.cc, data.num_fotos_cliente, data.data_nascimento],
-			function (error, rows, fields) {
+			function(error, rows, fields) {
 				if (error) throw error;
 				callback(rows[0]);
 			});
@@ -112,7 +131,7 @@ module.exports = {
 
 	remove(data, callback) {
 		var sql = "DELETE from clientes WHERE idclientes=?";
-		global.connection.query(sql, [data], function (error, rows, fields) {
+		global.connection.query(sql, [data], function(error, rows, fields) {
 			if (error) throw error;
 			callback(rows);
 		});
@@ -121,7 +140,7 @@ module.exports = {
 		var sql = "UPDATE clientes SET nome=?, morada=?, telemovel=?, email=?, nif=?, cc=?  WHERE idclientes=?";
 		global.connection.query(
 			sql, [data.nome, data.morada, data.telemovel, data.email, data.nif, data.cc, data.idclientes],
-			function (error, rows, fields) {
+			function(error, rows, fields) {
 				if (error) throw error;
 				callback(rows[0]);
 			});
